7f36b3877afbcf2c5fa53bbaa5c12e1b7d88485c
arthurcamargo03 <arthurcamargo561@gmail.com>
2026-06-24 23:09:08 -0300

    chore: bootstrap spring boot project

---
+++ src/main/java/com/treinotracker/TreinoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.treinotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinotracker/TreinoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
